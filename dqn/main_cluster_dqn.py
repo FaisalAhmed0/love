40df59986f84070792ab055d38b87e7f4be9275f
FaisalAhmed0 <faisal.ahmed.a.m@gmail.com>
2023-06-11 14:52:40 +0000

    added config_bindings to params

--- dqn/main_cluster_dqn.py
+++ dqn/main_cluster_dqn.py
@@ -5,12 +5,16 @@ import numpy as np
 from train_rl import Workspace
 from main import main
 from cluster import exit_for_resume, read_params_from_cmdline, save_metrics_params
+import json
 
 # os.environ['PYTHONPATH'] = '/home/fmohamed/love'
 
 
 def main_cluster():
+    with open("/home/fmohamed/love/cluster_settings/cfg_finetune.json") as f:
+        all_parma = json.load(f)
     params = read_params_from_cmdline()
+    params["config_bindings"] = all_parma["config_bindings"]
     print(f"params:{params}")
     exitcode = main(params)
     if exitcode == 3:
