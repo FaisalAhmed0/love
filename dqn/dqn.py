b2e511267c2ce2db76aaaa8b2be6fc5ae5c42824
FaisalAhmed0 <faisal.ahmed.a.m@gmail.com>
2023-06-16 23:21:05 +0000

    .

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -340,8 +340,9 @@ class DQNPolicy(nn.Module):
                     1, best_actions).squeeze(1)
         else:
             current_state_q_values = self._Q(states, actions)
-            print(f"next_states")
-            print(next_states)
+            # print(f"next_states")
+            # print(next_states)
+            next_states = torch.stack(next_states)
             best_actions = self.continuous_actor(next_states)
             next_state_q_values = self._target_Q(next_states, best_actions).squeeze(1)
             self.target_q.append(next_state_q_values.mean().cpu().item())
